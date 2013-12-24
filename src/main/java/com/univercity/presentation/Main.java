5f46ec5562e4d0484bc78f093a7803aef13a87e5
kbv7C9 <b.kasjanenko@yandex.ua>
2013-12-24 19:05:59 +0200

    Rejected commit: Default

--- src/main/java/com/univercity/presentation/Main.java
+++ src/main/java/com/univercity/presentation/Main.java
@@ -5,16 +5,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Acer
- * Date: 23.12.13
- * Time: 20:45
+ * Автор: Касьяненко Борис
+ * Это главный класс программы, которая ищет средние
+ * значения столбцов квадратной матрицы а также
+ * максимальное значение на побочной диагонали
  */
 public class Main {
 
     public static void main(String[] args){
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/applicationContext.xml");
-        MatrixProcess matrixProcess = (MatrixProcess)applicationContext.getBean("matrixProcessImpl");
+        MatrixProcess matrixProcess = (MatrixProcess)applicationContext.getBean("MatrixProcessImpl");
         double[] averageColumnResult = matrixProcess.averageColumn(new double[][]{{1,2,3},{4,5,6},{7,8,9}});
         double maxSideLineResult = matrixProcess.maxSideLine(new double[][]{{1,2,3},{4,5,6},{7,8,9}});
         System.out.print("\n Average values ​​in columns: \n");
